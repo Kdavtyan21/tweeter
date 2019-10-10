72ad6fdc2de540292409de218b4953344cc9b6d6
Karen <karendavtyan21@mail.com>
2019-10-10 19:32:29 +0000

    added time and reset of your text and letter count

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,12 +26,22 @@ const createTweetElement = function(tweet) {
   <article>
   <img class="avatar" src=${escape(tweet.user.avatars)}><a>${escape(tweet.user.name)}</a><a class="handle">${escape(tweet.user.handle)}</a>
   <header class="tweets">${escape(tweet.content.text)}</header>
-  <footer>${escape(tweet.created_at)}</footer>
+  <footer>${formatAMPM()}</footer>
   </article>
   </section>
   </section>`;
   return $tweet;
 };
+
+function formatAMPM() {
+  let d = new Date(),
+    minutes = d.getMinutes().toString().length ===1 ? '0' + d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length === 1 ? '0' + d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+  return days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ampm;
+}
 
 
 $(function() {
@@ -52,8 +62,9 @@ $(function() {
       method: 'POST', 
       data: $input,
       success: function() {
-        
         loadTweets()
+        $("#tweetText").val("")
+        $(".counter").text(140)
       }
     })
   }
